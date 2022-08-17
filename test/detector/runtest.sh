65250b4c7b56db5fba782cc90ddd23c39e333024
miktim <miktim@mail.ru>
2022-08-17 06:20:51 +0300

    0.0.2

--- test/detector/runtest.sh
+++ test/detector/runtest.sh
@@ -2,12 +2,12 @@
 
 echo $(javac -version)
 echo $(java -version)
-if [ -f ../../dist/java-charset-detector-0.0.0.jar ]; then
-  javac -cp ../../dist/java-charset-detector-0.0.0.jar DetectorTest.java
-  java  -cp ../../dist/java-charset-detector-0.0.0.jar:. DetectorTest
+if [ -f ../../dist/java-charset-detector-0.0.2.jar ]; then
+  javac -cp ../../dist/java-charset-detector-0.0.2.jar DetectorTest.java
+  java  -cp ../../dist/java-charset-detector-0.0.2.jar:. DetectorTest
   rm -f *.class
 else
-  echo First make the java-charset-detector-0.0.0.jar file.
+  echo First make the java-charset-detector-0.0.2.jar file.
 fi
 echo
 echo Completed. Press Enter to exit...
